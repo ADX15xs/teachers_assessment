194e874413efff1d4554fe40d233e175224a52c7
pw_xs <1007883921@qq.com>
2019-01-23 11:33:40 +0800

    koa2文档测试

---
+++ background/app.js
@@ -0,0 +1,28 @@
+// 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
+const Koa = require('koa');
+const app = new Koa();
+
+// 打印日志
+app.use(async (ctx, next) => {
+    console.log(`${ctx.request.method} ${ctx.request.url}`); // 打印URL
+    await next(); // 调用下一个middleware
+});
+
+// 记录时间
+app.use(async (ctx, next) => {
+    const start = new Date().getTime(); // 当前时间
+    await next(); // 调用下一个middleware
+    const ms = new Date().getTime() - start; // 耗费时间
+    console.log(`Time: ${ms}ms`); // 打印耗费时间
+});
+
+// 对于任何请求，app将调用该异步函数处理请求：
+app.use(async (ctx, next) => {
+    await next();
+    ctx.response.type = 'text/html';        // 设置response的Content-Type
+    ctx.body = 'Hello Ko2';                 // 设置response的内容
+});
+
+// 在端口3000监听:
+app.listen(3000);
+console.log('app started at port 3000...');
