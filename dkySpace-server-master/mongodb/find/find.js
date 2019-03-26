ecede7dedb3d51a3a12a142b995cd9e5fc5cb97b
sise214 <1007883921@qq.com>
2019-03-27 05:08:26 +0800

    dawkey个人博客后端

---
+++ dkySpace-server-master/mongodb/find/find.js
@@ -0,0 +1,63 @@
+const models = require("../init.js");
+
+// const connect = require("../connect.js");
+// connect();
+
+function find_view(name){
+  let model = models[name];
+  let promise = model.find({},{__v: 0},{sort: {_id: -1}});
+  return promise;
+}
+
+
+function find_use(){
+  let model = models["use_model"];
+  let promise = model.find({},{__v: 0});
+  return promise;
+}
+
+
+function find_article(_id){
+  let model = models["article_model"];
+  let promise = model.find({_id: _id},{__v: 0});
+  return promise;
+}
+
+function find_article_main(){
+  let model = models["main_model"];
+  let promise = model.find(
+    {},{__v: 0,description: 0},{sort: {_id: -1}}
+  );
+  return promise;
+}
+
+
+function find_draft(_id){
+  let model = models["draft_model"];
+  let promise = model.find({_id: _id},{__v: 0});
+  return promise;
+}
+
+function find_draft_main(){
+  let model = models["draft_model"];
+  let promise = model.find({},{__v: 0 , markdown: 0 , article: 0},{sort: {_id: -1}});
+  return promise;
+}
+
+
+function find_login(){
+  let model = models["login_model"];
+  let promise = model.find({},{_v: 0});
+  return promise;
+}
+
+
+module.exports = {
+  find_view,
+  find_use,
+  find_article,
+  find_article_main,
+  find_draft,
+  find_draft_main,
+  find_login
+};
